a251064f9f2fd7dfbcc31ebc2ac8ea71f3284e73
igsr5 <igsr5.dev@gmail.com>
2023-05-20 16:55:31 +0900

    implement webhook logic for message type

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,19 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.post('/line/message_api/webhook', (req, res) => {
+  const body = req.body;
+  // NOTE: 実際には event はmesasgeごとに複数届くので本番サービスでは何かしら考慮する必要があるが、今回は単純化のため最初のメッセージのみ考える
+  const event = body.events[0];
+
+  // NOTE: 実際にはメッセージにはテキスト形式だけでなく、画像などもくる。本番サービスでは何かしら考慮する必要があるが、今回は単純化のためテキストのみ考える
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // no-op
+    return;
+  }
+
   res.send('Ping!');
 });
 
